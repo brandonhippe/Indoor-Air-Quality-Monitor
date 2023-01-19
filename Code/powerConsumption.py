972c001e8faa157b282c28ab5c6f9d3beba4999b
brandonhippe <bhippe@pdx.edu>
2023-01-19 15:42:16 -0800

    Graphing added

--- Code/powerConsumption.py
+++ Code/powerConsumption.py
@@ -1,6 +1,7 @@
 from itertools import product
 from collections import defaultdict
 from math import ceil
+import matplotlib.pyplot as plt
 
 
 UNIT_PREFIXES = {'p': -12, 'n': -9, 'u': -6, 'm': -3}
@@ -58,17 +59,8 @@ def calcDutyCycle(dutyCycles):
 def calcPower(component):
     power = 0
     period = component["Time (On)"] + component["Time (Off)"]
-
-    if "Typical Power Consumption (On)" in component:
-        power += component["Typical Power Consumption (On)"] * component["Time (On)"] / period
-    else:
-        power += component["Voltage (On)"] * component["Typical Current (On)"] * component["Time (On)"] / period
-
-    if "Typical Power Consumption (Off)" in component:
-        power += component["Typical Power Consumption (Off)"] * component["Time (Off)"] / period
-    else:
-        power += component["Voltage (Off)"] * component["Typical Current (Off)"] * component["Time (Off)"] / period
-
+    power += component["Typical Power Consumption (On)"] * component["Time (On)"] / period
+    power += component["Typical Power Consumption (Off)"] * component["Time (Off)"] / period
     return power
 
 
@@ -113,25 +105,19 @@ def main():
 
     output = []
     for i in components:
+        for p in ["On", "Off"]:
+            if f"Typical Power Consumption ({p})" not in components[i]:
+                components[i][f"Typical Power Consumption ({p})"] = components[i][f"Voltage ({p})"] * components[i][f"Typical Current ({p})"]
+
         if "Time (Off)" in components[i]:
-            if "Typical Power Consumption (Off)" in components[i]:
-                p1 = calcPower(components[i])
+            p1 = calcPower(components[i])
 
-                powerInfo = components[i]["Typical Power Consumption (Off)"]
-                components[i]["Typical Power Consumption (Off)"] = 0
+            powerInfo = components[i]["Typical Power Consumption (Off)"]
+            components[i]["Typical Power Consumption (Off)"] = 0
 
-                p2 = calcPower(components[i])
+            p2 = calcPower(components[i])
 
-                components[i]["Typical Power Consumption (Off)"] = powerInfo
-            elif "Voltage (Off)" in components[i]:
-                p1 = calcPower(components[i])
-
-                voltageInfo = components[i]["Voltage (Off)"]
-                components[i]["Voltage (Off)"] = 0
-
-                p2 = calcPower(components[i])
-                
-                components[i]["Voltage (Off)"] = voltageInfo
+            components[i]["Typical Power Consumption (Off)"] = powerInfo
 
             output.append(f"{i}: Sleep power accounts for {(p1 - p2) / p1 * 100} % of total used power.")
 
@@ -141,6 +127,49 @@ def main():
     for i in range(len(groupings) - 1, -1, -1):
         if len(groupings[i]) == 0:
             groupings.pop(i)
+
+    fig = plt.figure()
+    gs = fig.add_gridspec(3, 2, hspace=0)
+    axs = gs.subplots()
+    for i in range(1, 3):
+        for j in range(2):
+            axs[i, j].sharex(axs[i - 1, j])
+            axs[i, j].sharey(axs[i - 1, j])
+
+    leftTitle, rightTitle = "", ""
+    for g in groupings[1:]:
+        leftTitle += f"{components[g[0]]['Type']} Power Consumption vs Sampling Rate (Logarithmic)\n"
+        rightTitle += f"{components[g[0]]['Type']} Sampling Rate vs Power Consumption (Logarithmic)\n"
+
+    axs[0, 0].set_title(leftTitle[:-1])
+    axs[0, 1].set_title(rightTitle[:-1])
+
+    axs[1, 0].set_ylabel("Power Consumption (Watts)")
+    axs[1, 1].set_ylabel("Sampling Rate (Samples/Hour)")
+    axs[2, 0].set_xlabel("Sampling Rate (Samples/Hour)")
+    axs[2, 1].set_xlabel("Power Consumption (Watts)")
+
+    for i, g in enumerate(groupings[1:]):
+        for item in g:
+            x, y = [], []
+
+            offTime = components[item]["Time (Off)"]
+            for t in range(int(components[item]["Time (On)"]), 3601):
+                x.append(3600 / t)
+                components[item]["Time (Off)"] = t - components[item]["Time (On)"]
+                y.append(calcPower(components[item]))
+
+            components[item]["Time (Off)"] = offTime
+
+            axs[i, 0].loglog(x, y, label = item)
+            axs[i, 1].loglog(y, x, label = item)
+
+        axs[i, 0].legend()
+        axs[i, 1].legend()
+
+
+    plt.get_current_fig_manager().window.state('zoomed')
+    fig.show()
 
     power = defaultdict(lambda: defaultdict(lambda: 0))
     cost = defaultdict(lambda: 0)
@@ -169,7 +198,6 @@ def main():
         batterySize[items] = sum(power[items].values()) * 24
 
     powerSorted = sorted(power.keys(), key=lambda e: sum(power[e].values()))
-    costSorted = sorted(cost.keys(), key=lambda e: cost[e])
 
     output.append("Configurations sorted by lowest power consumption:")
     for i, config in enumerate(powerSorted):
@@ -179,30 +207,9 @@ def main():
 
         output.append(f"    Power Consumption: {sum(power[config].values())} watts")
         output.append(f"    Cost: ${cost[config]}")
-        output.append(f"    Battery Size for 1 Day battery life: {batterySize[config]} watt-hours")
-        output.append(f"    Battery Size for 1 Week battery life: {batterySize[config] * 7} watt-hours")
-        output.append(f"    Battery Size for 1 Month battery life: {batterySize[config] * 365 / 12} watt-hours")
-        output.append(f"    Battery Size for 1 Year battery life: {batterySize[config] * 365} watt-hours")
+        output.append(f"    Battery Life on single 18650 cell: {12.6 / batterySize[config]} days")
         minCells = ceil(batterySize[config] * 365 / 12.6)
-        minCells -= minCells - ceil(minCells / 2) * 2
-        output.append(f"    18650 Cells required for 1 year battery life: {minCells} cells")
-        output.append(f"    Battery Life with minimum 18650 cells: {minCells * 12.6 / batterySize[config]} days")
-
-
-    output.append("\nConfigurations sorted by lowest cost:")
-    for i, config in enumerate(costSorted):
-        output.append(f"{i + 1}.")
-        for item in config:
-            output.append(f"    {components[item]['Type']}: {item} ({power[config][item] / sum(power[config].values()) * 100} %)")
-
-        output.append(f"    Power Consumption: {sum(power[config].values())} watts")
-        output.append(f"    Cost: ${cost[config]}")
-        output.append(f"    Battery Size for 1 Day battery life: {batterySize[config]} watt-hours")
-        output.append(f"    Battery Size for 1 Week battery life: {batterySize[config] * 7} watt-hours")
-        output.append(f"    Battery Size for 1 Month battery life: {batterySize[config] * 365 / 12} watt-hours")
-        output.append(f"    Battery Size for 1 Year battery life: {batterySize[config] * 365} watt-hours")
-        minCells = ceil(batterySize[config] * 365 / 12.6)
-        minCells -= minCells - ceil(minCells / 2) * 2
+        minCells += minCells % 2
         output.append(f"    18650 Cells required for 1 year battery life: {minCells} cells")
         output.append(f"    Battery Life with minimum 18650 cells: {minCells * 12.6 / batterySize[config]} days")
 
@@ -210,7 +217,8 @@ def main():
         with open("output.txt", "w") as f:
             f.write('\n'.join(output))
 
-    print('\n'.join(output))
+    for o in output:
+        print(o)
 
 
 if __name__ == "__main__":
