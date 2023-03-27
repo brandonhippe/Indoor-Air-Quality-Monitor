e6c83422e3b8e541ae58fbaa08c95cde66c85b58
brandonhippe <bhippe@pdx.edu>
2023-03-26 22:01:18 -0600

    Modified ultrasonic pulse generation code

--- Code/Ultrasonic_Sensor_Test/Ultrasonic_Sensor_Test.ino
+++ Code/Ultrasonic_Sensor_Test/Ultrasonic_Sensor_Test.ino
@@ -3,25 +3,29 @@
 #define echo2 14
 
 
-#define halfD 0.125
+const float halfD = 0.05;
 
 
 unsigned int ultrasonicPulse(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(1000);
+  delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   return pulseIn(echoPin, HIGH);
 }
 
 
 float calcWindSpeed(unsigned int forward, unsigned int back) {
-  float invForward = 1 / forward, invBack = 1 / back;
+  float invForward = 1000000.0 / float(forward);
+  float invBack = 1000000.0 / float(back);
   return halfD * (invForward - invBack);
 }
 
 
 float calcTemperature(unsigned int forward, unsigned int back) {
-  float invForward = 1 / forward, invBack = 1 / back;
+  float invForward = 1000000.0 / float(forward);
+  float invBack = 1000000.0 / float(back);
   return halfD * (invForward + invBack);
 }
 
@@ -37,10 +41,14 @@ void setup() {
 void loop() {
   unsigned int forward = ultrasonicPulse(trig, echo1);
   delay(100);
-  unsigned int back = ultrasonicPulse(trig, echo2);
-  float windspeed = calcWindSpeed(forward, back), temperature = calcTemperature(forward, back);
-  char printstring[100];
-  sprintf(printstring, "Wind speed: %f m/s\nTemperature: %f degrees\n", windspeed, temperature);
-  Serial.print(printstring);
-  delay(900);
+//  unsigned int back = ultrasonicPulse(trig, echo2);
+  Serial.println(forward);
+//  Serial.println(back);
+//  float windspeed = calcWindSpeed(forward, back), temperature = calcTemperature(forward, back);
+//  char windStr[10], tempStr[10];
+//  dtostrf(windspeed, 2, 2, windStr);
+//  dtostrf(temperature, 2, 2, tempStr); 
+//  char printstring[100];
+//  sprintf(printstring, "Wind speed: %s m/s\nTemperature: %s degrees", windStr, tempStr);
+//  Serial.println(printstring);
 }
