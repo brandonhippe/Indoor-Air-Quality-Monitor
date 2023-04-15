a241aee94d11d8d69c02549ad93968422b4e6d66
brandonhippe <bhippe@pdx.edu>
2023-04-15 09:35:07 -0700

    Added Anemometer library

---
+++ Code/libraries/CGAnem/CGAnem.cpp
@@ -0,0 +1,104 @@
+#include "CGAnem.h"
+
+
+const uint8_t dev_id = 0x05, windMSB = 0x07, windLSB = 0x08;
+
+
+CGAnem::CGAnem() {
+	max_clock = 200000;
+	period_ms = 60000;
+	measurement_ready = false;
+}
+
+
+boolean CGAnem::begin(int sleepPin, uint64_t currTime_ms, boolean _debug) {
+	uint32_t startTime = millis();
+	debug = _debug;
+	sleep_pin = sleepPin;
+	digitalWrite(sleep_pin, LOW);
+	pinMode(sleep_pin, OUTPUT);
+	
+	
+	Wire.beginTransmission(ADDR);
+	Wire.write(dev_id);
+	if (Wire.endTransmission() != 0) {
+		time_ms = 0xFFFFFFFF;
+		return false;
+	}
+	
+	while (Wire.available() == 0);
+	Wire.read();
+	
+	// Put sensor to sleep
+	digitalWrite(sleep_pin, HIGH);	
+
+	// Schedule Measurement
+	scheduledFunc = START_MEASUREMENT;
+	time_ms = currTime_ms + period_ms + millis() - startTime;
+  
+	return true;
+}
+
+
+void CGAnem::startNextFunc(uint64_t currTime_ms) {
+	switch (scheduledFunc) {
+		case START_MEASUREMENT:
+			start_measurement(currTime_ms);
+			break;
+		case FINISH_MEASUREMENT:
+			finish_measurement(currTime_ms);
+			break;
+	}
+}
+
+
+void CGAnem::start_measurement(uint64_t currTime_ms) {
+	uint32_t startTime = millis();
+	lastMeasurement = currTime_ms;
+	measurement_ready = false;
+	
+	// Wakeup sensor
+	if (debug) {
+		Serial.println("CGAnem: Start Measurement");
+	}
+	
+	digitalWrite(sleep_pin, LOW);
+	
+	// Schedule measurement finish
+	scheduledFunc = FINISH_MEASUREMENT;
+	time_ms = currTime_ms + 10000 + millis() - startTime;
+}
+
+
+void CGAnem::finish_measurement(uint64_t currTime_ms) {
+	uint32_t startTime = millis();
+	measurement_ready = true;
+	
+	// Get measurement from sensor
+	if (debug) {
+		Serial.println("CGAnem: Finish Measurement");
+	}
+	
+	Wire.beginTransmission(ADDR);
+	Wire.write(windMSB);
+	Wire.endTransmission(false);
+	Wire.requestFrom(ADDR, 1);
+	
+	while (Wire.available() == 0);
+	wind = (Wire.read()) << 8;
+	
+	Wire.beginTransmission(ADDR);
+	Wire.write(windLSB);
+	Wire.endTransmission(false);
+	Wire.requestFrom(ADDR, 1);
+	
+	while (Wire.available() == 0);
+	wind += 8;
+	
+	// Put sensor to sleep
+	digitalWrite(sleep_pin, HIGH);
+	
+	// Schedule next measurement
+	scheduledFunc = START_MEASUREMENT;
+	time_ms = lastMeasurement + period_ms;
+}
---
+++ Code/libraries/CGAnem/CGAnem.h
@@ -0,0 +1,36 @@
+// Include necessary libraries
+#include <NewWire.h>
+#include <stdint.h>
+#include "Energia.h"
+
+
+// I2C Address and Function Selection definitions
+#define ADDR 0x11
+
+#define START_MEASUREMENT 0
+#define FINISH_MEASUREMENT 1
+
+
+
+// Declare CGAnem class
+class CGAnem {
+	public:
+		// Public Variables
+		uint32_t period_ms, max_clock;
+		uint64_t time_ms;
+		uint16_t wind;
+		boolean debug;
+		
+		// Public Functions
+		CGAnem();
+		boolean begin(uint64_t currTime_ms, boolean _debug);
+		void startNextFunc(uint64_t currTime_ms);
+	private:
+		// Private Variables
+		uint64_t lastMeasurement;
+		int scheduledFunc, sleep_pin;
+		
+		// Private Functions
+		void start_measurement(uint64_t currTime_ms);
+		void finish_measurement(uint64_t currTime_ms);
+};
